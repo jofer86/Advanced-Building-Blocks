1d267811674991749e65aa6cb742a44325e1b446
Jorge Fernando Rincón Figueroa <jofer86@gmail.com>
2019-04-10 17:53:55 -0500

    finished the yield bubble sort, some minor implementation needed

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -18,4 +18,24 @@ print bubble_sort(["cat", "mouse", "rabbit", "c'thulu", "pikachu"])
 puts
 
 print bubble_sort([4, 5, 2, 6, 8, 7, 1, 102, 5, 3, 45, 85])
+puts 
+puts "--------------------------------"
+
+def bubble_sort_by(arr)
+	sorted = false
+	while sorted == false
+		sorted = true
+		(0...arr.length-1).each do |i|
+			if yield(arr[i], arr[i+1]) > 0
+				arr[i], arr[i+1] = arr[i+1], arr[i]
+				sorted = false
+			end
+		end
+	end
+	return arr
+end
+
+
+
+print bubble_sort_by(["hi","hello","hey", "waddup", "exquize me", "dance", "revolution of the machines"]) {|left,right| left.length - right.length}
 puts

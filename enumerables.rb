7ff9d43b206c70b62570ce1a8ee40c34a283edec
Jorge Fernando Rincón Figueroa <jofer86@gmail.com>
2019-04-11 15:47:48 -0500

    Complete my map method and test.

--- enumerables.rb
+++ enumerables.rb
@@ -19,8 +19,6 @@ module Enumerable
     self.my_each do |ele|
       if yield(ele) == true
         arr << ele
-      else
-        ele
       end
     end
     arr
@@ -31,8 +29,6 @@ module Enumerable
     self.my_each do |ele|
       if yield(ele) == false
         return false
-      else
-        ele
       end
     end
     all
@@ -43,8 +39,6 @@ module Enumerable
     self.my_each do |ele|
       if yield(ele) == true
         return true
-      else
-        ele
       end
     end
     any
@@ -55,13 +49,28 @@ module Enumerable
     self.my_each do |ele|
       if yield(ele) == true
         return false
-      else
-        ele
       end
     end
     none
   end
 
+  def my_count
+    count = 0
+    self.my_each do |ele|
+      if yield(ele) == true
+      count += 1
+      end
+    end
+    count
+  end
+
+  def my_map
+    arr = []
+    self.my_each do |ele|
+      arr << yield(ele)
+    end
+    arr
+  end
 
 end
 
@@ -75,35 +84,49 @@ end
 
 arr = [5, 8, 9, 4, 7, 6, 8, 10]
 # Test my_each
-puts 'Test my each'
+puts 'Test my_each'
 puts  arr.my_each { |ele| puts ele * 2 }
 puts '------------------------------'
 
 # Test my_each_with_index
-puts 'Test each with index'
+puts 'Test my_each_with_index'
 arr.my_each_with_index { |ele, idx| puts "#{ele} is in index #{idx}" }
 puts '------------------------------'
 
 # Test my_select
-puts 'test my select'
+puts 'test my_select'
 my_s = arr.my_select{ |ele| ele.even? }
-puts my_s
+print my_s
+puts
 puts '------------------------------'
 
 # Test my_all?
-puts 'test my all'
+puts 'test my_all?'
 puts arr.my_all?{ |n| n == 0 }
 puts arr.my_all?{ |n| n.is_a? Integer}
 puts '------------------------------'
 
 # Test my_any?
-puts 'test my any'
+puts 'test my_any?'
 puts arr.my_any? { |n| n == 8}
 puts arr.my_any? { |n| n == 154}
 puts '------------------------------'
 
-#Test my_none?"
-
+# Test my_none?"
+puts 'Test my_none'
 puts arr.my_none? { |n| n == 1 }
 puts arr.my_none? { |n| n == 8 }
+puts '------------------------------'
 
+# Test my_count
+puts 'Test my_count'
+puts arr.my_count { |n| n.even? }
+puts arr.my_count { |n| n == 8 }
+puts '------------------------------'
+
+# Test my_map
+puts 'Test my_map'
+my_m = arr.my_map { |n| n * 2}
+print my_m
+puts
+puts '------------------------------'

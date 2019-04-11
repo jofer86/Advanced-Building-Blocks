e32f2ea6b8394ca3d855446729c3638acd8f752e
Jorge Fernando Rincón Figueroa <jofer86@gmail.com>
2019-04-11 13:58:27 -0500

    rubocop on bubble sort project

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,38 +3,37 @@ def bubble_sort(array)
 
   while sorted == false
     sorted = true
-    (0...array.length-1).each do |i|
-      if array[i] > array[i+1]
-        array[i], array[i+1] = array[i+1], array[i]
+    (0...array.length - 1).each do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
         sorted = false
       end
     end
   end
-  return array
+  array
 end
 
-print bubble_sort(["cat", "mouse", "rabbit", "c'thulu", "pikachu"])
+print bubble_sort(['cat', 'mouse', 'rabbit', 'c\'thulu', 'pikachu'])
 puts
 
 print bubble_sort([4, 5, 2, 6, 8, 7, 1, 102, 5, 3, 45, 85])
-puts 
-puts "--------------------------------"
+puts
+puts '--------------------------------'
 
 def bubble_sort_by(arr)
-	sorted = false
-	while sorted == false
-		sorted = true
-		(0...arr.length-1).each do |i|
-			if yield(arr[i], arr[i+1]) > 0
-				arr[i], arr[i+1] = arr[i+1], arr[i]
-				sorted = false
-			end
-		end
-	end
-	return arr
+  sorted = false
+  while sorted == false
+    sorted = true
+    (0...arr.length - 1).each do |i|
+      if yield(arr[i], arr[i + 1]) > 0
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        sorted = false
+      end
+    end
+  end
+  arr
 end
 
-
-
-print bubble_sort_by(["hi","hello","hey", "waddup", "exquize me", "dance", "revolution of the machines"]) {|left,right| left.length - right.length}
+print bubble_sort_by(['hi', 'hello', 'hey', 'waddup', 'exquize me',
+                      'dance', 'revolution of the machines']) { |left, right| left.length - right.length }
 puts

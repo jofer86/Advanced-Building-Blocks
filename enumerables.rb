a4c0d1f2daab654418e58abb22bb68762db892ea
Jorge Fernando Rincón Figueroa <jofer86@gmail.com>
2019-04-11 15:06:33 -0500

    complete my none? method, tested and works.

--- enumerables.rb
+++ enumerables.rb
@@ -38,6 +38,31 @@ module Enumerable
     all
   end
 
+  def my_any?
+    any = false
+    self.my_each do |ele|
+      if yield(ele) == true
+        return true
+      else
+        ele
+      end
+    end
+    any
+  end
+
+  def my_none?
+    none = true
+    self.my_each do |ele|
+      if yield(ele) == true
+        return false
+      else
+        ele
+      end
+    end
+    none
+  end
+
+
 end
 
 
@@ -50,14 +75,35 @@ end
 
 arr = [5, 8, 9, 4, 7, 6, 8, 10]
 # Test my_each
-arr.my_each { |ele| puts ele * 2 }
+puts 'Test my each'
+puts  arr.my_each { |ele| puts ele * 2 }
+puts '------------------------------'
 
 # Test my_each_with_index
+puts 'Test each with index'
 arr.my_each_with_index { |ele, idx| puts "#{ele} is in index #{idx}" }
+puts '------------------------------'
 
 # Test my_select
+puts 'test my select'
 my_s = arr.my_select{ |ele| ele.even? }
 puts my_s
+puts '------------------------------'
 
 # Test my_all?
+puts 'test my all'
 puts arr.my_all?{ |n| n == 0 }
+puts arr.my_all?{ |n| n.is_a? Integer}
+puts '------------------------------'
+
+# Test my_any?
+puts 'test my any'
+puts arr.my_any? { |n| n == 8}
+puts arr.my_any? { |n| n == 154}
+puts '------------------------------'
+
+#Test my_none?"
+
+puts arr.my_none? { |n| n == 1 }
+puts arr.my_none? { |n| n == 8 }
+

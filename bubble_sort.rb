d0026ebcd48b4785fa841779112ac7a80f5541e6
Jorge Fernando Rincón Figueroa <jofer86@gmail.com>
2019-04-11 14:52:48 -0500

    add case in which block is not given.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -13,27 +13,19 @@ def bubble_sort(array)
   array
 end
 
-print bubble_sort(['cat', 'mouse', 'rabbit', 'c\'thulu', 'pikachu'])
-puts
-
-print bubble_sort([4, 5, 2, 6, 8, 7, 1, 102, 5, 3, 45, 85])
-puts
-puts '--------------------------------'
-
 def bubble_sort_by(arr)
   sorted = false
   while sorted == false
     sorted = true
     (0...arr.length - 1).each do |i|
-      if yield(arr[i], arr[i + 1]) > 0
+      if block_given?
+        yield(arr[i], arr[i + 1]) > 0
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
         sorted = false
+      else
+        return 'No block given'
       end
     end
   end
   arr
 end
-
-print bubble_sort_by(['hi', 'hello', 'hey', 'waddup', 'exquize me',
-                      'dance', 'revolution of the machines']) { |left, right| left.length - right.length }
-puts
